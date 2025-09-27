fd916a0e6b9bb3905116d1814794c908fea7a8aa
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-09-28 00:57:28 +0530

    fix: format

--- forms_pro/install.py
+++ forms_pro/install.py
@@ -3,33 +3,33 @@ import frappe
 
 
 def after_install() -> None:
-	try:
-		create_forms_pro_roles()
-	except Exception as e:
-		click.secho(f"Error creating Forms Pro roles: {e}", bold=True, fg="red")
-		raise
+    try:
+        create_forms_pro_roles()
+    except Exception as e:
+        click.secho(f"Error creating Forms Pro roles: {e}", bold=True, fg="red")
+        raise
 
 
 def create_forms_pro_roles() -> None:
-	click.secho("Creating Forms Pro roles 📝", bold=True)
+    click.secho("Creating Forms Pro roles 📝", bold=True)
 
-	if frappe.db.exists("Role", "Forms Pro User"):
-		click.secho("Forms Pro roles already exist ✅", bold=True)
-		return
+    if frappe.db.exists("Role", "Forms Pro User"):
+        click.secho("Forms Pro roles already exist ✅", bold=True)
+        return
 
-	roles = [
-		{
-			"role_name": "Forms Pro User",
-		},
-	]
+    roles = [
+        {
+            "role_name": "Forms Pro User",
+        },
+    ]
 
-	for role in roles:
-		frappe.get_doc(
-			{
-				"doctype": "Role",
-				"role_name": role.get("role_name"),
-				"desk_access": role.get("desk_access", 0),
-			}
-		).insert()
+    for role in roles:
+        frappe.get_doc(
+            {
+                "doctype": "Role",
+                "role_name": role.get("role_name"),
+                "desk_access": role.get("desk_access", 0),
+            }
+        ).insert()
 
-	click.secho("Forms Pro roles created ✅", bold=True)
+    click.secho("Forms Pro roles created ✅", bold=True)
