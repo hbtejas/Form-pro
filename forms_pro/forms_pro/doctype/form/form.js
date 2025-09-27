4e65fb31da0b19e27023e03f04f7c6dbdd694b51
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-09-27 19:52:24 +0530

    feat: init Form doctype

---
+++ forms_pro/forms_pro/doctype/form/form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, harsh@buildwithhussain.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Form", {
+// 	refresh(frm) {
+
+// 	},
+// });
