1e3a52d0491bfab9dbfdfa05bad0149ea086125e
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-10-06 02:46:46 +0530

    chore!: setup typescript

--- frontend/src/utils/theme.js
+++
@@ -1,5 +0,0 @@
-export default function toggleTheme() {
-	const root = document.documentElement;
-	const theme = root.getAttribute("data-theme");
-	root.setAttribute("data-theme", theme === "light" ? "dark" : "light");
-}
