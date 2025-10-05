27b7d0fcf963c7654a6a2f9c68126b436e7f2a03
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-10-06 02:47:43 +0530

    chore: integrate Pinia for state management

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,41 +1,44 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
 import { initSocket } from "./socket";
 
 import {
-	Alert,
-	Badge,
-	Button,
-	Dialog,
-	ErrorMessage,
-	FormControl,
-	Input,
-	TextInput,
-	frappeRequest,
-	pageMetaPlugin,
-	resourcesPlugin,
-	setConfig,
+    Alert,
+    Badge,
+    Button,
+    Dialog,
+    ErrorMessage,
+    FormControl,
+    Input,
+    TextInput,
+    frappeRequest,
+    pageMetaPlugin,
+    resourcesPlugin,
+    setConfig,
 } from "frappe-ui";
 
 import "./index.css";
 
 const globalComponents = {
-	Button,
-	TextInput,
-	Input,
-	FormControl,
-	ErrorMessage,
-	Dialog,
-	Alert,
-	Badge,
+    Button,
+    TextInput,
+    Input,
+    FormControl,
+    ErrorMessage,
+    Dialog,
+    Alert,
+    Badge,
 };
 
 const app = createApp(App);
+const pinia = createPinia();
 
 setConfig("resourceFetcher", frappeRequest);
 
+app.use(pinia);
 app.use(router);
 app.use(resourcesPlugin);
 app.use(pageMetaPlugin);
@@ -44,7 +47,7 @@ const socket = initSocket();
 app.config.globalProperties.$socket = socket;
 
 for (const key in globalComponents) {
-	app.component(key, globalComponents[key]);
+    app.component(key, globalComponents[key]);
 }
 
 app.mount("#app");
