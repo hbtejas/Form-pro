7d0635a0f5cf560b9e91f446743f2cec73ab5d51
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-10-06 18:04:53 +0530

    feat: add route for editing forms with dynamic ID parameter

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,33 +3,38 @@ import { createRouter, createWebHistory } from "vue-router";
 import { session } from "./data/session";
 
 const routes = [
-	{
-		path: "/",
-		name: "Dashboard",
-		component: () => import("@/pages/Dashboard.vue"),
-	},
+    {
+        path: "/",
+        name: "Dashboard",
+        component: () => import("@/pages/Dashboard.vue"),
+    },
+    {
+        path: "/edit-form/:id",
+        name: "Edit Form",
+        component: () => import("@/pages/EditForm.vue"),
+    },
 ];
 
 const router = createRouter({
-	history: createWebHistory("/forms"),
-	routes,
+    history: createWebHistory("/forms"),
+    routes,
 });
 
 router.beforeEach(async (to, from, next) => {
-	let isLoggedIn = session.isLoggedIn;
-	try {
-		await userResource.promise;
-	} catch (error) {
-		isLoggedIn = false;
-	}
+    let isLoggedIn = session.isLoggedIn;
+    try {
+        await userResource.promise;
+    } catch (error) {
+        isLoggedIn = false;
+    }
 
-	if (to.name === "Login" && isLoggedIn) {
-		next({ name: "Home" });
-	} else if (to.name !== "Login" && !isLoggedIn) {
-		window.location.href = `/login?redirect-to=/forms${to.fullPath}`;
-	} else {
-		next();
-	}
+    if (to.name === "Login" && isLoggedIn) {
+        next({ name: "Home" });
+    } else if (to.name !== "Login" && !isLoggedIn) {
+        window.location.href = `/login?redirect-to=/forms${to.fullPath}`;
+    } else {
+        next();
+    }
 });
 
 export default router;
