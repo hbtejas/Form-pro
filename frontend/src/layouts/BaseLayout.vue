79ff04ff894c85bf9eea9e4e43911f361719aab1
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-10-07 12:49:44 +0530

    refactor: clean up BaseLayout.vue

--- frontend/src/layouts/BaseLayout.vue
+++ frontend/src/layouts/BaseLayout.vue
@@ -1,15 +1,15 @@
 <template>
-	<div class="flex h-screen w-full">
-		<Sidebar
-			:header="{
-				title: 'Forms Pro',
-				subtitle: session.full_name,
-				menuItems: menuItems,
-			}"
-			:sections="sidebarSections"
-		/>
-		<slot></slot>
-	</div>
+    <div class="flex h-screen w-full">
+        <Sidebar
+            :header="{
+                title: 'Forms Pro',
+                subtitle: session.full_name,
+                menuItems: menuItems,
+            }"
+            :sections="sidebarSections"
+        />
+        <slot></slot>
+    </div>
 </template>
 <script setup>
 import { session } from "@/data/session";
@@ -18,39 +18,39 @@ import { Sidebar } from "frappe-ui";
 import toggleTheme from "@/utils/theme";
 
 const menuItems = computed(() => {
-	return [
-		{
-			label: "Toggle Theme",
-			icon: "moon",
-			onClick: toggleTheme,
-		},
-		{
-			label: "Log out",
-			icon: "log-out",
-			onClick: session.logout.submit,
-		},
-	];
+    return [
+        // {
+        // 	label: "Toggle Theme",
+        // 	icon: "moon",
+        // 	onClick: toggleTheme,
+        // },
+        {
+            label: "Log out",
+            icon: "log-out",
+            onClick: session.logout.submit,
+        },
+    ];
 });
 
 const sidebarSections = ref([
-	{
-		label: "",
-		items: [
-			{
-				label: "Dashboard",
-				to: "/",
-				isActive: true,
-			},
-		],
-	},
-	{
-		label: "",
-		items: [
-			{
-				label: "Submissions",
-				to: "/submissions",
-			},
-		],
-	},
+    {
+        label: "",
+        items: [
+            {
+                label: "Dashboard",
+                to: "/",
+                isActive: true,
+            },
+        ],
+    },
+    // {
+    // 	label: "",
+    // 	items: [
+    // 		{
+    // 			label: "Submissions",
+    // 			to: "/submissions",
+    // 		},
+    // 	],
+    // },
 ]);
 </script>
