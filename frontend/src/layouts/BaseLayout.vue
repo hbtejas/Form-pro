161f364d82e5c1fc15f5289ef696b87a0cf248e0
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-12-03 00:33:10 +0530

    feat: enhance BaseLayout with custom header logo and sidebar icon

--- frontend/src/layouts/BaseLayout.vue
+++ frontend/src/layouts/BaseLayout.vue
@@ -7,7 +7,15 @@
                 menuItems: menuItems,
             }"
             :sections="sidebarSections"
-        />
+        >
+            <template #header-logo>
+                <div
+                    class="font-instrument font-bold bg-surface-white rounded border border-outline-gray-1 py-[2px]"
+                >
+                    F.
+                </div>
+            </template>
+        </Sidebar>
         <slot></slot>
     </div>
 </template>
@@ -15,6 +23,7 @@
 import { session } from "@/data/session";
 import { ref, computed } from "vue";
 import { Sidebar } from "frappe-ui";
+import { LayoutDashboard } from "lucide-vue-next";
 import toggleTheme from "@/utils/theme";
 
 const menuItems = computed(() => {
@@ -40,6 +49,7 @@ const sidebarSections = ref([
                 label: "Dashboard",
                 to: "/",
                 isActive: true,
+                icon: LayoutDashboard,
             },
         ],
     },
