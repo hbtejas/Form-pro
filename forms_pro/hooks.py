5e0c9b53744d4d6581c2178484029868639bd230
Harsh Tandiya <harsh.tandiya@gmail.com>
2026-01-06 20:50:48 +0530

    fix: routing

--- forms_pro/hooks.py
+++ forms_pro/hooks.py
@@ -241,5 +241,5 @@ export_python_type_annotations = True
 
 
 website_route_rules = [
-    {"from_route": "/frontend/<path:app_path>", "to_route": "frontend"},
+    {"from_route": "/forms/<path:app_path>", "to_route": "forms"},
 ]
